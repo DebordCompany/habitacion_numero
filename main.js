5bb2218f5dad1d7d7b3d440fc4746b1b1b906a6d
DebordCompany <debord.info@gmail.com>
2022-11-10 15:24:03 +0100

    Inclusion switch para cambio de idioma, todavia no esta resuelto en js

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
+console.log("hola mundo");
+
+const idioma = document.querySelector("#switch_selector");
+
+const textsToChange = document.querySelectorAll('[data-section]')
+
+const changeLanguaje = async languaje=>{
+
+    const requestJson = await fetch(`./assets/languaje/${languaje}.json`)
+    const texts = await requestJson.json();
+    
+   textsToChange.forEach(element => {
+        const section = element.dataset.section;
+        const value = element.dataset.value;
+        element.innerHTML = texts
+        console.log(section, value)
+
+    });
 
 
-
-console.log("hola mundo")
+}
+idioma.addEventListener("change", (e)=>{
+   
+    if(idioma.checked){
+        idioma.dataset.languaje="en"
+        changeLanguaje(idioma.dataset.languaje);
+        
+        
+    }
+    else{
+        idioma.dataset.languaje="es"
+        changeLanguaje(idioma.dataset.languaje);
+        
+    }
+    
+})
