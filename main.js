9bdf7a8a99a6ac0daafee1cb4c8cb3aa4b0e398a
DebordCompany <debord.info@gmail.com>
2022-11-10 15:53:35 +0100

    resuelto

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("hola mundo");
 
+
 const idioma = document.querySelector("#switch_selector");
 
 const textsToChange = document.querySelectorAll('[data-section]')
@@ -9,28 +10,31 @@ const changeLanguaje = async languaje=>{
     const requestJson = await fetch(`./assets/languaje/${languaje}.json`)
     const texts = await requestJson.json();
     
-   textsToChange.forEach(element => {
+   /*textsToChange.forEach(element => {
         const section = element.dataset.section;
         const value = element.dataset.value;
         element.innerHTML = texts
         console.log(section, value)
 
-    });
+    });*/
+    for(const item of textsToChange){
+        const section = item.dataset.section;
+        const value = item.dataset.value;
+        item.textContent = texts[section][value];
+        console.log(section, value)
+    }
 
 
 }
-idioma.addEventListener("change", (e)=>{
+
+idioma.addEventListener("change", ()=>{
    
-    if(idioma.checked){
-        idioma.dataset.languaje="en"
-        changeLanguaje(idioma.dataset.languaje);
-        
-        
-    }
-    else{
-        idioma.dataset.languaje="es"
-        changeLanguaje(idioma.dataset.languaje);
-        
-    }
+   if(idioma.checked){
+    idioma.dataset.languaje = "en"
+   }
+   else{
+    idioma.dataset.languaje = "es"
+   }
     
+    changeLanguaje(idioma.dataset.languaje);
 })
